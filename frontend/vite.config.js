926171896d40be015ebb8eaddb186e7ccdfc308d
Aranpra <aryaandreanpratama@students.amikom.ac.id>
2025-07-13 15:46:42 +0700

    fix(deploy): Resolve 404 errors on Vercel deploymentv2

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,6 +10,19 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  server: {
+    proxy: {
+      // Semua permintaan yang dimulai dengan '/api'
+      '/api': {
+        // Akan diteruskan ke server backend-mu
+        target: 'http://localhost:3000',
+        // Mengubah origin header agar sesuai dengan target
+        changeOrigin: true,
+        // Jangan menulis ulang path, biarkan '/api/skills' tetap '/api/skills'
+        // rewrite: (path) => path.replace(/^\/api/, '') // Baris ini TIDAK diperlukan untuk kasusmu
+      }
+    }
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
